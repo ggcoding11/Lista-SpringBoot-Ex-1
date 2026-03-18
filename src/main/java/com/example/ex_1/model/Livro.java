e6170111ef6712cd9861ae0a0e72ecf2fee4e6ff
Gustavo <gustavogomes1108@gmail.com>
2026-03-17 21:41:57 -0300

    add-model

---
+++ src/main/java/com/example/ex_1/model/Livro.java
@@ -0,0 +1,28 @@
+package com.example.lista_exercicios.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="tb_livro")
+public class Livro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="coluna_titulo")
+    private String titulo;
+
+    @Column(name="coluna_autor")
+    private String autor;
+
+    @Column(name="coluna_ano_publicacao")
+    private Integer anoPublicacao;
+}
