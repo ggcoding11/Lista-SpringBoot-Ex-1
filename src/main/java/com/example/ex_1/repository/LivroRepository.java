80234276c132be72254e9edb56e2085e35f94c81
Gustavo <gustavogomes1108@gmail.com>
2026-03-17 21:42:23 -0300

    add-repository

---
+++ src/main/java/com/example/ex_1/repository/LivroRepository.java
@@ -0,0 +1,9 @@
+package com.example.lista_exercicios.repository;
+
+import com.example.lista_exercicios.model.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Long> {
+}
